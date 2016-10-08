34c9418ad3f7a0190628ae76b1ba9c01b0ba0fcf
xsu <xsu@thoughtworks.com>
2016-10-08 16:36:16 +0800

    Initial commit

---
+++ src/main/java/spring/Main.java
@@ -0,0 +1,17 @@
+package spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by xlo on 16/2/26.
+ * It's the main class
+ */
+@SpringBootApplication()
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
